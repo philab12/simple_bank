075b4a74acb41a480708790ab2a14f9b56ead4be
Philip Gyamfi <philab.pg@gmail.com>
2024-10-30 14:02:52 +0000

    update connection string

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:pass@172.22.77.119:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:pass@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
